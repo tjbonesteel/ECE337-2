7e8bf0efeff8b451b2a4cfe314cf046571676f9d
Mentor Graphics <mg88@ecegrid-lnx.ecn.purdue.edu>
2013-09-16 19:15:48 -0400

    finish avg_four

--- Lab4/source/avg_four.sv
+++ Lab4/source/avg_four.sv
@@ -6,8 +6,30 @@ module avg_four
   input wire data_ready,
   output wire one_k_samples,
   output wire modwait,
-  output wire [15:0] avg_out,
+  output reg [15:0] avg_out,
   output wire err
   );
   
-  //division block = bitshift right 2x 
+  reg dr;
+  reg cnt_up;
+  reg [1:0] op;
+  reg [3:0] src1;
+  reg [3:0] src2;
+  reg [3:0] dest;
+  reg overflow;
+  reg [15:0] tmp_out;
+  
+  
+  sync I(.clk(clk), .n_reset(n_reset), .async_in(data_ready), .sync_out(dr));
+  
+  controller II(.clk(clk), .n_reset(n_reset), .dr(dr), .overflow(overflow), .cnt_up(cnt_up), .modwait(modwait), .op(op), .src1(src1), .src2(src2), .dest(dest), .err(err));
+    
+  counter III(.clk(clk), .n_reset(n_reset), .cnt_up(cnt_up), .one_k_samples(one_k_samples));
+  
+  datapath IV(.clk(clk), .n_reset(n_reset), .ext_data(sample_data), .op(op), .src1(src1), .src2(src2), .dest(dest), .overflow(overflow), .outreg_data(tmp_out));
+
+  always @ (posedge clk) begin
+    avg_out <= tmp_out >> 2;
+  end
+  
+endmodule
