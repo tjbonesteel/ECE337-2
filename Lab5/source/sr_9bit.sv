80b27bfe00bb3c0b3a94ffcb3d7259a65c91365e
Mentor Graphics <mg88@ecegrid-lnx.ecn.purdue.edu>
2013-09-23 18:43:57 -0400

    wrote initinal timer and sr_9bit blocks

---
+++ Lab5/source/sr_9bit.sv
@@ -0,0 +1,36 @@
+module sr_9bit
+  (
+  input wire clk,
+  input wire n_rst,
+  input wire shift_strobe,
+  input wire serial_in,
+  output wire [7:0] packet_data,
+  output wire stop_bit
+  );
+  
+  reg [8:0] data;
+  reg enable;
+  reg count = 0;
+  
+  stp_sr #(9,1) SHIFT(.clk(clk), .n_rst(n_rst), .shift_enable(enable), .serial_in(serial_in), .output_data(data));
+  
+    
+  assign packet_data[7:0] = data[7:0];
+  assign stop_bit = data[8];
+  assign shift_strobe = (count >= 9)? 1'b1: 1'b0;
+  
+  always @ (clk, n_rst) begin
+    
+    if(1'b0 == n_rst) begin
+      data <= 1'b0;
+      enable <=1'b0;
+      count <= 1'b0;
+    end else begin
+      data <= data;
+      enable <=1'b1;
+      count = count +1;
+    end
+    
+  end
+  
+endmodule
---
+++ Lab5/source/timer.sv
@@ -0,0 +1,39 @@
+module timer
+  (
+  input wire clk,
+  input wire n_rst,
+  input wire enable_timer,
+  output wire shift_strobe,
+  output wire packet_done
+  );
+  
+  reg value;
+  reg done = 1'b0;
+  int i;
+  
+  
+  flex_counter CNT(.clk(clk), .n_rst(n_rst), .count_enable(enable_timer), .rollover_val(value), .rollover_flag(shift_strobe));
+  
+  assign packet_done = done;
+  
+  
+  always @ (clk, n_rst) begin
+    
+    if(1'b0 == n_rst) begin
+      value <= 1'b0;
+      done <= 1'b0;
+    end else begin
+      
+      value = 5;
+      
+      for(i=0;i<9;i=i+1) begin
+        value = 10;
+      end
+      
+      done <= 1'b1;
+      
+    end
+    
+  end
+  
+endmodule
